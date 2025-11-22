6bf0d0b060fb87d1daf760b2e595a874df65b739
Ribhav Revalli <ribsai@outlook.com>
2025-11-21 22:11:53 -0500

    Updated System: Fri Nov 21 10:11:53 PM EST 2025

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,7 @@
   python314
   guacamole-server
   git
+  git-lfs
   htop
   nmap
   rsync
@@ -96,6 +97,7 @@
   neovim
   nodePackages_latest.nodejs
   virtualbox
+  kalker
      ];
    };
 
@@ -131,5 +133,27 @@ virtualisation.virtualbox.host.enable = true;
 users.extraGroups.vboxusers.members = [ "ribhav" ];
 virtualisation.virtualbox.guest.enable = true;
 virtualisation.virtualbox.guest.dragAndDrop = true;
+hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      # Shows battery charge of connected devices on supported
+      # Bluetooth adapters. Defaults to 'false'.
+      Experimental = true;
+      # When enabled other devices can connect faster to us, however
+      # the tradeoff is increased power consumption. Defaults to
+      # 'false'.
+      FastConnectable = true;
+    };
+    Policy = {
+      # Enable all controllers when they are found. This includes
+      # adapters present on start as well as adapters that are plugged
+      # in later on. Defaults to 'true'.
+      AutoEnable = true;
+    };
+  };
+};
+
 
 }
