e5eab6ad176a7f77057fc8d5d7713699af57609c
Ribhav Revalli <ribsai@outlook.com>
2026-07-05 17:42:10 -0400

    Added Gatus

--- nixos-modules/gatus.nix
+++ nixos-modules/gatus.nix
@@ -66,7 +66,7 @@
                 conditions = [
                   " [STATUS] == 200"
                   "[BODY].status == up"
-                ]
+                ];
             }
       ];
     };
