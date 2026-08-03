33a321aa4700de0950f0287a3347d2da47ff7ab9
Ribhav Revalli <ribsai@outlook.com>
2026-08-03 12:38:59 -0400

    Big Update: Changed from KDE -> XFCE -> LxQT

--- nixos-modules/desktop.nix
+++ nixos-modules/desktop.nix
@@ -10,15 +10,14 @@
   services.printing.enable = true;
 
   # --- LXQt Wayland Setup ---
-  # Enable LXQt desktop environment
-  services.desktopManager.lxqt.enable = true;
+  services.xserver = {
+    enable = true; # Required for NixOS to load the desktopManager module
+    desktopManager.lxqt.enable = true;
+  };
 
   # Expose the native LXQt Wayland session to the login manager
   services.displayManager.sessionPackages = [ pkgs.lxqt.lxqt-wayland-session ];
 
   # Use Ly: a lightweight, console-based desktop manager that handles Wayland perfectly
   services.displayManager.ly.enable = true;
-
-  # Explicitly disable the legacy X11 server completely
-  services.xserver.enable = false;
 }
